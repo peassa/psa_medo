b3cb617d982753ccf60b78f56688b23f264aa1b4
peassa <thepeassa@gmail.com>
2020-12-01 02:50:06 +0300

    psa_medo

---
+++ server/main.lua
@@ -0,0 +1,9 @@
+RegisterCommand("me", function(source , args , rawCommand)
+    args = table.concat(args, ' ')
+    TriggerClientEvent('me:event', -1, source, args, 0)
+end)
+
+RegisterCommand("do", function(source , args , rawCommand)
+    args = table.concat(args, ' ')
+    TriggerClientEvent('me:event', -1, source, args, 1)
+end)
